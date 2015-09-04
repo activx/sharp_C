aa6c7ef92f57dc1a705faaacf7d1b37b124e0ba0
activx <ilbaev@ator.com.ua>
2015-09-04 21:06:45 +0300

    zadanie3

---
+++ zanyatie3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zanyatie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество чисел в массиве: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] Array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Введите число " + (i + 1) + ": ");
+                Array[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            quicksort(Array, 0, size - 1);
+            // Отпечатаем массив
+            for (int i = 0; i < size; i++) Console.WriteLine("Элемент " + (i) + ":" + Array[i]);
+        }
+        /**
+         * Делитель массива на части
+         **/
+        static int partition(int[] array, int start, int end)
+        {
+            int marker = start;
+            for (int i = start; i <= end; i++)
+            {
+                if (array[i] <= array[end])
+                {
+                    int temp = array[marker];
+                    array[marker] = array[i];
+                    array[i] = temp;
+                    marker += 1;
+                }
+            }
+            return marker - 1;
+        }
+
+
+        /// <summary>
+        /// Быстрая сортировка (ОСТОРОЖНО РАБОТАЕТ В РЕКУРСИИ)
+        /// </summary>
+        /// <param name="array">Массив для перебора</param>
+        /// <param name="start">Начальный элемент сортировки </param>
+        /// <param name="end">Конечный элемент сортировки</param>
+        static void quicksort(int[] array, int start, int end)
+        {
+
+            if (start >= end)
+            {
+                return;
+            }
+
+            int marker = partition(array, start, end); // Делим массив на части
+            quicksort(array, start, marker - 1); // Сортируем до маркера
+            quicksort(array, marker + 1, end); // Сортируем после маркера
+        }
+    }
+}
+ 
